abec42fa675e35784b7ce5419c375bb1ccaa17dc
github username <github email address>
2020-07-22 21:54:40 -0700

    Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Router>
+      <div className="header">
+        <Navbar />
+      </div>
+
+      <Switch>
+
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/" component={Home} />
+
+        </Switch>
+
+    </Router>
     </div>
   );
 }
